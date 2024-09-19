657c65f0a93a46620827b51379cee38a72ec6bba
Alba <alba.garza@imaginelearning.com>
2024-09-19 13:22:24 -0600

    Set up docker

---
+++ codesynthesis-training-backend/src/main/java/com/example/codesynthesis_training_backend/CodesynthesisTrainingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.codesynthesis_training_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodesynthesisTrainingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodesynthesisTrainingBackendApplication.class, args);
+	}
+
+}
---
+++ codesynthesis-training-backend/src/test/java/com/example/codesynthesis_training_backend/CodesynthesisTrainingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codesynthesis_training_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodesynthesisTrainingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
